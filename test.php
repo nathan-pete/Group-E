ae0200b64eb978387f898688e2411e28c1afc60e
TheLobster1 <rjeveldman@hotmail.nl>
2021-10-13 13:11:51 +0200

    Create test.php
    Created test.php and added basic elements

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8">
+        <title>Test</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
