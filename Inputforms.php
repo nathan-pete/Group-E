10cd01fc61b3d5c2001417e9421c2120a42b4594
Nathan P. Pais D'costa <71150111+nathan-pete@users.noreply.github.com>
2021-10-25 15:15:18 +0200

    Yes

---
+++ Inputforms.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <title>Sign up!</title>
+        <meta charset="UTF-8">
+        <link rel='stylesheet' type='text/css' media='screen' href='Style_Gallery.css'>
+    </head>
+    <body>
+         <div class="container">
+             
+         </div>
+        <?php
+        
+        ?>
+    </body>
+</html>
