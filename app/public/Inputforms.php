20445714a589612d6efd3844eb71d8cd2d237558
Nathan P. Pais D'costa <71150111+nathan-pete@users.noreply.github.com>
2021-10-27 15:11:01 +0200

    Update

--- app/public/Inputforms.php
+++ app/public/Inputforms.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Subscribe</title>
-    <link rel="stylesheet" href="formstyle.css">
+    <link rel="stylesheet" href="style/css/formstyle.css">
     <link rel="icon" href="NHL Stenden.png">
 </head>
 <body>
