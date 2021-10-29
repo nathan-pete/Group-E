02194979f1c8765d9dcb9913b0b2b9d5669b8191
TheLobster1 <rjeveldman@hotmail.nl>
2021-10-29 16:47:55 +0200

    folder structure
    Reworked folder structure and am experimenting with extraction of files

--- app/public/fileextraction.php
+++ app/public/fileextraction.php
@@ -7,18 +7,41 @@
     <body>
         <?php
        
-       $title = file_get_contents("TextDocs\\Titles\\Title1.txt");
-       echo "$title <br>";
+       //$title = file_get_contents("TextDocs\\Titles\\Title1.txt");
+       //echo "$title <br>";
 
-       $date = file_get_contents("TextDocs\\Dates\\Date1.txt");
-       echo "$date <br>";
+      // $date = file_get_contents("TextDocs\\Dates\\Date1.txt");
+      // echo "$date <br>";
 
-       $shortdesc = file_get_contents("TextDocs\\ShortDesc\\ShortDesc1.txt");
-       echo "$shortdesc <br>";
+      // $shortdesc = file_get_contents("TextDocs\\ShortDesc\\ShortDesc1.txt");
+       //echo "$shortdesc <br>";
 
-       $desc = file_get_contents("TextDocs\\Desc\\Desc1.txt");
-       echo "$desc <br>";
+      // $desc = file_get_contents("TextDocs\\Desc\\Desc1.txt");
+       //echo "$desc <br>";
 
+
+//       $directory = 'TextDocs';
+
+//    if (!is_dir($directory)) {
+ //       exit('Invalid diretory path');
+ //   }
+
+ //   $files = array();
+ //   foreach (scandir($directory) as $file) {
+ //       if ($file !== '.' && $file !== '..') {
+  //          $files[] = $file;
+  //      }
+  //      $testing = file_get_contents("TextDocs\\$file");
+  //      echo "$testing <br>";
+  //  }
+  $fileSystemIterator = new FilesystemIterator('TextDocs');
+
+  $entries = array();
+  foreach ($fileSystemIterator as $fileInfo){
+      $entries[] = $fileInfo->getFilename();
+      echo "$entries";
+  }
+  var_dump($entries);
         ?>
     </body>
 </html>
