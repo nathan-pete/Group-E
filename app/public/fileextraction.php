892b70a15aa05ecfa5d6ff6bfc0d1d9ae71016b1
TheLobster1 <rjeveldman@hotmail.nl>
2021-10-31 20:31:55 +0100

    Update fileextraction.php

--- app/public/fileextraction.php
+++ app/public/fileextraction.php
@@ -7,32 +7,35 @@
     <body>
         <?php
        
-       $file = file_get_contents("TextDocs\\1.txt");
-       echo "$file <br>";
+//       $file = file_get_contents("TextDocs\\1.txt");
+//       echo "$file <br>";
 
 
-//       $directory = 'TextDocs';
+       $directory = 'TextDocs';
+      
 
-//    if (!is_dir($directory)) {
- //       exit('Invalid diretory path');
- //   }
-
- //   $files = array();
- //   foreach (scandir($directory) as $file) {
- //       if ($file !== '.' && $file !== '..') {
-  //          $files[] = $file;
-  //      }
-  //      $testing = file_get_contents("TextDocs\\$file");
-  //      echo "$testing <br>";
-  //  }
-  $fileSystemIterator = new FilesystemIterator('TextDocs');
-
-  $entries = array();
-  foreach ($fileSystemIterator as $fileInfo){
-      $entries[] = $fileInfo->getFilename();
+    $files = array();
+    foreach (scandir($directory) as $file) {
+        if ($file !== '.' && $file !== '..') {
+            $files[] = $file;
+        }
+        $testing = file_get_contents("TextDocs\\$files");
+        
+    echo "$testing";
+    echo "<br>";
     
-  }
-  var_dump($entries);
+        
+    }
+//  $fileSystemIterator = new FilesystemIterator('TextDocs');
+//
+//  $entries = array();
+//  foreach ($fileSystemIterator as $fileInfo){
+//      $entries[] = $fileInfo->getFilename();
+//        echo "$entries";
+//  }
+//  var_dump($entries);
+$files = array_slice(scandir('TextDocs'), 2);
+echo "$files";
         ?>
     </body>
 </html>
